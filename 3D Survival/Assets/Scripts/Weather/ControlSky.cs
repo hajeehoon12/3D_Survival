9d6159ed2bcdc547e3f028fee728b815376e4971
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-03 18:41:50 +0900

    *Weather Refactoring

--- 3D Survival/Assets/Scripts/Weather/ControlSky.cs
+++ 3D Survival/Assets/Scripts/Weather/ControlSky.cs
@@ -6,18 +6,28 @@ public class ControlSky : MonoBehaviour
 {
     public Material dayMat;
     public Material nightMat;
+
     public GameObject dayLight;
+    public GameObject dayEffect;
     public GameObject nightLight;
+    public GameObject nightEffect;
 
     public Material snowyMat;
     public Material rainyMat;
+
     public GameObject snowyLight;
+    public GameObject snowyEffect;
     public GameObject rainyLight;
+    public GameObject rainyEffect;
 
     public Material sunsetMat;
     public Material sunriseMat;
+
+
     public GameObject sunsetLight;
+    public GameObject sunsetEffect;
     public GameObject sunriseLight;
+    public GameObject sunriseEffect;
 
     public Color dayFog;
     public Color nightFog;
@@ -40,84 +50,162 @@ public class ControlSky : MonoBehaviour
         rainyLight.SetActive(false);
         sunsetLight.SetActive(false);
         sunriseLight.SetActive(false);
+
+        dayEffect.SetActive(true);
+        nightEffect.SetActive(false);
+        snowyEffect.SetActive(false);
+        rainyEffect.SetActive(false);
+        sunsetEffect.SetActive(false);
+        sunriseEffect.SetActive(false);
+
     }
     void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * 2.0f);
     }
 
+    void IfDay()
+    {
+        RenderSettings.skybox = dayMat;
+        RenderSettings.fogColor = dayFog;
+        dayLight.SetActive(true);
+        nightLight.SetActive(false);
+        snowyLight.SetActive(false);
+        rainyLight.SetActive(false);
+        sunsetLight.SetActive(false);
+        sunriseLight.SetActive(false);
+
+        dayEffect.SetActive(true);
+        nightEffect.SetActive(false);
+        snowyEffect.SetActive(false);
+        rainyEffect.SetActive(false);
+        sunsetEffect.SetActive(false);
+        sunriseEffect.SetActive(false);
+    }
+
+    void IfNight()
+    {
+        RenderSettings.skybox = nightMat;
+        RenderSettings.fogColor = nightFog;
+        dayLight.SetActive(false);
+        nightLight.SetActive(true);
+        snowyLight.SetActive(false);
+        rainyLight.SetActive(false);
+        sunsetLight.SetActive(false);
+        sunriseLight.SetActive(false);
+
+        dayEffect.SetActive(false);
+        nightEffect.SetActive(true);
+        snowyEffect.SetActive(false);
+        rainyEffect.SetActive(false);
+        sunsetEffect.SetActive(false);
+        sunriseEffect.SetActive(false);
+    }
+
+    void IfSnowy()
+    {
+        RenderSettings.skybox = snowyMat;
+        RenderSettings.fogColor = snowyFog;
+        dayLight.SetActive(false);
+        nightLight.SetActive(false);
+        snowyLight.SetActive(true);
+        rainyLight.SetActive(false);
+        sunsetLight.SetActive(false);
+        sunriseLight.SetActive(false);
+
+        dayEffect.SetActive(false);
+        nightEffect.SetActive(false);
+        snowyEffect.SetActive(true);
+        rainyEffect.SetActive(false);
+        sunsetEffect.SetActive(false);
+        sunriseEffect.SetActive(false);
+    }
+
+    void IfRainy()
+    {
+        RenderSettings.skybox = rainyMat;
+        RenderSettings.fogColor = rainyFog;
+        dayLight.SetActive(false);
+        nightLight.SetActive(false);
+        snowyLight.SetActive(false);
+        rainyLight.SetActive(true);
+        sunsetLight.SetActive(false);
+        sunriseLight.SetActive(false);
+
+        dayEffect.SetActive(false);
+        nightEffect.SetActive(false);
+        snowyEffect.SetActive(false);
+        rainyEffect.SetActive(true);
+        sunsetEffect.SetActive(false);
+        sunriseEffect.SetActive(false);
+    }
+    void IfSunset()
+    {
+        RenderSettings.skybox = sunsetMat;
+        RenderSettings.fogColor = sunsetFog;
+        dayLight.SetActive(false);
+        nightLight.SetActive(false);
+        snowyLight.SetActive(false);
+        rainyLight.SetActive(false);
+        sunsetLight.SetActive(true);
+        sunriseLight.SetActive(false);
+
+        dayEffect.SetActive(false);
+        nightEffect.SetActive(false);
+        snowyEffect.SetActive(false);
+        rainyEffect.SetActive(false);
+        sunsetEffect.SetActive(true);
+        sunriseEffect.SetActive(false);
+    }
+
+    void IfSunRise()
+    {
+        RenderSettings.skybox = sunriseMat;
+        RenderSettings.fogColor = sunriseFog;
+        dayLight.SetActive(false);
+        nightLight.SetActive(false);
+        snowyLight.SetActive(false);
+        rainyLight.SetActive(false);
+        sunsetLight.SetActive(false);
+        sunriseLight.SetActive(true);
+
+        dayEffect.SetActive(true);
+        nightEffect.SetActive(false);
+        snowyEffect.SetActive(false);
+        rainyEffect.SetActive(false);
+        sunsetEffect.SetActive(false);
+        sunriseEffect.SetActive(true);
+    }
+
     void OnGUI()
     {
 
-
-
-        if (GUI.Button(new Rect(5, 5, 80, 20), "Day")) {
-            RenderSettings.skybox = dayMat;
-            RenderSettings.fogColor = dayFog;
-            dayLight.SetActive(true);
-            nightLight.SetActive(false);
-            snowyLight.SetActive(false);
-            rainyLight.SetActive(false);
-            sunsetLight.SetActive(false);
-            sunriseLight.SetActive(false);
+        if (GUI.Button(new Rect(5, 5, 80, 20), "Day")) 
+        {
+            IfDay();
         }
 
         if (GUI.Button(new Rect(5, 35, 80, 20), "Night"))
         {
-            RenderSettings.skybox = nightMat;
-            RenderSettings.fogColor = nightFog;
-            dayLight.SetActive(false);
-            nightLight.SetActive(true);
-            snowyLight.SetActive(false);
-            rainyLight.SetActive(false);
-            sunsetLight.SetActive(false);
-            sunriseLight.SetActive(false);
+            IfNight();
         }
 
         if (GUI.Button(new Rect(5, 65, 80, 20), "Snowy"))
         {
-            RenderSettings.skybox = snowyMat;
-            RenderSettings.fogColor = snowyFog;
-            dayLight.SetActive(false);
-            nightLight.SetActive(false);
-            snowyLight.SetActive(true);
-            rainyLight.SetActive(false);
-            sunsetLight.SetActive(false);
-            sunriseLight.SetActive(false);
+            IfSnowy();
         }
         if (GUI.Button(new Rect(5, 95, 80, 20), "Rainy"))
         {
-            RenderSettings.skybox = rainyMat;
-            RenderSettings.fogColor = rainyFog;
-            dayLight.SetActive(false);
-            nightLight.SetActive(false);
-            snowyLight.SetActive(false);
-            rainyLight.SetActive(true);
-            sunsetLight.SetActive(false);
-            sunriseLight.SetActive(false);
+            IfRainy();
         }
 
         if (GUI.Button(new Rect(5, 125, 80, 20), "Sunset"))
         {
-            RenderSettings.skybox = sunsetMat;
-            RenderSettings.fogColor = sunsetFog;
-            dayLight.SetActive(false);
-            nightLight.SetActive(false);
-            snowyLight.SetActive(false);
-            rainyLight.SetActive(false);
-            sunsetLight.SetActive(true);
-            sunriseLight.SetActive(false);
+            IfSunset();
         }
         if (GUI.Button(new Rect(5, 155, 80, 20), "Sunrise"))
         {
-            RenderSettings.skybox = sunriseMat;
-            RenderSettings.fogColor = sunriseFog;
-            dayLight.SetActive(false);
-            nightLight.SetActive(false);
-            snowyLight.SetActive(false);
-            rainyLight.SetActive(false);
-            sunsetLight.SetActive(false);
-            sunriseLight.SetActive(true);
+            IfSunRise();
         }
     }
 
