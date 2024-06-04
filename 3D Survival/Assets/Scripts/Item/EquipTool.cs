430f4bc6d4e5ec605635c5bc8ca18e8ca62a538d
TunnelSight <167070589+TunnelSight@users.noreply.github.com>
2024-06-04 15:39:33 +0900

    *EquipTool.cs
    added collider check: TryGetComponent(out Monster_Zombie_Pattern1 monster_Zombie_Pattern1

--- 3D Survival/Assets/Scripts/Item/EquipTool.cs
+++ 3D Survival/Assets/Scripts/Item/EquipTool.cs
@@ -89,6 +89,12 @@ public class EquipTool : Equip
                     AudioManager.instance.PlaySFX("SwordAttack2", 0.8f);
                     isAttacking = false;
                 }
+                else if (hit.collider.TryGetComponent(out Monster_Zombie_Pattern1 monster_Zombie_Pattern1)) // Hit Monster_Zombie
+                {
+                    monster_Zombie_Pattern1.TakePhysicalDamage(damage);
+                    AudioManager.instance.PlaySFX("SwordAttack2", 0.8f);
+                    isAttacking = false;
+                }
             }
             
         }
