eaebbc70022f3bbb3229479a5793e68d608d6638
TunnelSight <167070589+TunnelSight@users.noreply.github.com>
2024-06-10 08:40:40 +0900

    *DialogueData.cs

--- 3D Survival/Assets/Scripts/ScriptableObject/DialogueData.cs
+++ 3D Survival/Assets/Scripts/ScriptableObject/DialogueData.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue")]
-public class Dialogue : ScriptableObject
+[CreateAssetMenu(fileName = "NewDialogue", menuName = "New Dialogue")]
+public class DialogueData : ScriptableObject
 {
-    [TextArea(3, 10)]
-    public string[] sentences;
+    [TextArea(1, 10)] public string[] sentences;
+    [TextArea(1, 10)] public string[] sentenceAccepted;
+    [TextArea(1, 10)] public string[] sentenceCleared;
+
 }
