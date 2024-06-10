4274bb5aeaea3390d0cedb4adc9035ca33a50154
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-10 15:19:00 +0900

    +Quest End

--- 3D Survival/Assets/Scripts/Quest/Quest.cs
+++ 3D Survival/Assets/Scripts/Quest/Quest.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class Quest : MonoBehaviour, IInteractable
 {
+
+    public static Quest Instance;
 
     public GameObject[] _Tombs;
     public GameObject Tombs;
     public GameObject _Zombie;
     public ItemData _rewardItem;
+    private int targetNum = 0;
+    private int MaxTargetNum;
 
-    private void Start()
+    private void Awake()
     {
-        //QuestStart();
+        Instance = this;
     }
 
 
     public void QuestStart()
     {
+        MaxTargetNum = _Tombs.Length;
         CoffinDown();
         DayNightCycle.instance._controlSky.IfRainy();
         DayNightCycle.instance.totalTime = 0f;
@@ -41,11 +47,13 @@ public class Quest : MonoBehaviour, IInteractable
 
     private void CoffinDown()
     {
+        AudioManager.instance.PlaySFX("ScaryBird", 0.5f);
         transform.DOMoveY(transform.position.y - 4 , 3f).onComplete += StartTombAnimation;
     }
 
     private void StartTombAnimation()
     {
+        AudioManager.instance.PlaySFX("ScaryTitle", 0.5f);
         AudioManager.instance.PlayBGM("Tapdance", 0.3f);
         
         Tombs.transform.DOScale(2, 5f);
@@ -59,10 +67,11 @@ public class Quest : MonoBehaviour, IInteractable
                 zombie.transform.DOMoveY(0.6f, 3f);
                 
             }
+            AudioManager.instance.PlaySFX("ScaryBell",0.5f);
         }
         );
 
-        RewardItem();//Temp
+       
     }
 
     private void EndTombAnimation()
@@ -73,7 +82,19 @@ public class Quest : MonoBehaviour, IInteractable
 
     }
 
+    public void TargetMission()
+    {
+        targetNum += 1;
+        if (targetNum == MaxTargetNum) EndQuest();
 
+    }
+
+    private void EndQuest()
+    {
+        EndTombAnimation();
+        AudioManager.instance.PlayBGM("Peace", 0.2f);
+        RewardItem();
+    }
 
     public void OnInteract()
     {
