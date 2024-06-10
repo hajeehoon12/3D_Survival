c6a2ce77feb4d104b722e5a4430c0e263f243149
TunnelSight <167070589+TunnelSight@users.noreply.github.com>
2024-06-10 18:28:00 +0900

    *QuestUI

--- 3D Survival/Assets/Scripts/Manager/QuestUIManager.cs
+++ 3D Survival/Assets/Scripts/Manager/QuestUIManager.cs
@@ -1,38 +1,61 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestUIManager : MonoBehaviour
-{
-    public GameObject questListPanel; // 할당: QuestListPanel
-    public GameObject questDetailPanel; // 할당: QuestDetailPanel
-    public GameObject questListItemPrefab; // 할당: QuestListItem 프리팹
-    public Transform questListContent; // 할당: QuestListPanel/Viewport/Content
-    public TextMeshProUGUI noQuestsMessage; // 할당: QuestListPanel/NoQuestsMessage
+{   
+    public static QuestUIManager instance;
+    public static QuestUIManager Instance
+    {
+        get 
+        {
+            if (instance == null)
+            { 
+                instance = new GameObject("QuestUIManager").AddComponent<QuestUIManager>();
+            }
+            return instance;
+        }
+    }
 
-    public TextMeshProUGUI questTitleText; // 할당: QuestDetailPanel/QuestTitleText
-    public TextMeshProUGUI questDescriptionText; // 할당: QuestDetailPanel/QuestDescriptionText
-    public TextMeshProUGUI questConditionText; // 할당: QuestDetailPanel/QuestConditionText
-    public TextMeshProUGUI questRewardText; // 할당: QuestDetailPanel/QuestRewardText
-    public TextMeshProUGUI questStatusText; // 할당: QuestDetailPanel/QuestStatusText
+    public GameObject questUI;
+    public GameObject questListPanel;
+    public GameObject questDetailPanel;
+    public GameObject questListItemPrefab;
+    public Transform questListContent;
+    public TextMeshProUGUI noQuestsMessage;
+
+    public TextMeshProUGUI questTitleText;
+    public TextMeshProUGUI questDescriptionText;
+    public TextMeshProUGUI questConditionText;
+    public TextMeshProUGUI questRewardText;
+    public TextMeshProUGUI questStatusText;
 
     private QuestController questController;
     
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance == this)
+            { 
+                Destroy(gameObject);
+            }
+        }
+    }
+
     void Start()
     {
         questController = CharacterManager.Instance.Player.GetComponent<QuestController>();
-        questListPanel.SetActive(false);
-        questDetailPanel.SetActive(false);
-        PopulateQuestList();
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ToggleQuestList();
-        }
+        Debug.Log(questController == null);
+        Debug.Log("??");
     }
 
     void PopulateQuestList()
@@ -50,7 +73,7 @@ public class QuestUIManager : MonoBehaviour
             {
                 hasQuests = true;
                 GameObject questListItem = Instantiate(questListItemPrefab, questListContent);
-                questListItem.GetComponentInChildren<Text>().text = quest.title;
+                questListItem.GetComponentInChildren<TextMeshProUGUI>().text = quest.title;
                 Button questButton = questListItem.GetComponent<Button>();
                 questButton.onClick.AddListener(() => DisplayQuestDetails(quest));
             }
@@ -65,10 +88,29 @@ public class QuestUIManager : MonoBehaviour
         questListPanel.SetActive(false);
 
         questTitleText.text = quest.title;
-        questDescriptionText.text = quest.dialogueData.sentencesNotStarted.Length > 0 ? quest.dialogueData.sentencesNotStarted[0] : "No Description";
-        questConditionText.text = quest.conditionText;
-        questRewardText.text = quest.rewardText;
-        questStatusText.text = quest.questStatus.ToString();
+        questDescriptionText.text = String.Format($"{quest.descriptionText.Replace("\\n", "\n")}");
+        questConditionText.text = String.Format($"클리어 조건: {quest.conditionText.Replace("\\n", "\n")}");
+        questRewardText.text = String.Format($"보상: {quest.rewardText}");
+        questStatusText.text = StatusToString(quest.questStatus);
+
+    }
+
+    public string StatusToString(QuestStatus Status)
+    {   
+        string str="default";
+
+        switch(Status)
+        {
+            case QuestStatus.NotStarted:
+            case QuestStatus.InProgress:
+            str = "(진행중)";
+            break;
+            case QuestStatus.Completed:
+            str = "(완료됨)";
+            break;
+            
+        }
+        return str;
     }
 
     public void CloseQuestDetail()
@@ -79,15 +121,51 @@ public class QuestUIManager : MonoBehaviour
 
     public void ToggleQuestList()
     {
-        if (questListPanel.activeSelf || questDetailPanel.activeSelf)
+        if (!questListPanel.activeSelf && questDetailPanel.activeSelf)
         {
-            questListPanel.SetActive(false);
+            questListPanel.SetActive(true);
             questDetailPanel.SetActive(false);
+            PopulateQuestList();
         }
         else
         {
+            questListPanel.SetActive(false);
+            questDetailPanel.SetActive(true);
+            
+        }
+    }
+
+    public void ToggleUI()
+    {
+        if (questUI.gameObject.activeSelf)
+        {
+            TurnOff();
+        }
+        else
+        {
+            TurnOn();
             questListPanel.SetActive(true);
             PopulateQuestList();
         }
     }
+
+    public void TurnOn()
+    {   
+        questUI.gameObject.SetActive(true);
+        questDetailPanel.SetActive(false);
+        AudioManager.instance.PlaySFX("Inventory");
+        Cursor.lockState = CursorLockMode.None;
+        CharacterManager.Instance.Player.controller.canLook = false;
+    }
+
+    public void TurnOff()
+    {   
+        questUI.gameObject.SetActive(false);
+        questListPanel.SetActive(false);
+        questDetailPanel.SetActive(false);
+        AudioManager.instance.PlaySFX("InventoryOff");
+        Cursor.lockState = CursorLockMode.Locked;
+        CharacterManager.Instance.Player.controller.canLook = true;
+    }
+
 }
