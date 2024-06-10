be5216c05b157af2d5e46792e5e2527457536c7c
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-10 11:55:06 +0900

    +Scene View

---
+++ 3D Survival/Assets/Scripts/Quest/Quest.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Quest : MonoBehaviour, IInteractable
+{
+
+    public GameObject[] _Tombs;
+    public GameObject Tombs;
+
+    private void Start()
+    {
+        //QuestStart();
+    }
+
+
+    public void QuestStart()
+    {
+        CoffinDown();
+        Debug.Log("Quest Start!!");
+    
+    }
+
+    public (string, string) GetInteractPrompt()
+    {
+        string str = $"꺼림찍한 관";
+        string str1 = $"불길한 기운이 스며나오고 있는 기분나쁜 관이다.";
+        return (str, str1);
+    }
+
+    private void CoffinDown()
+    {
+        transform.DOMoveY(transform.position.y - 4 , 3f).onComplete += TombAnimation;
+    }
+
+    private void TombAnimation()
+    {
+        Tombs.transform.DOScale(1, 10f);
+        Tombs.transform.DORotate(new Vector3(1080, 0 , 0), 10f);
+    }
+
+    public void OnInteract()
+    {
+        QuestStart();
+    }
+}
